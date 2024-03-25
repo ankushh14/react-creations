a84d04611b6740f254eb3122fc4bd77a067481b9
ankushh14 <ankushshenoy97@gmail.com>
2024-03-26 05:08:30 +0530

    feat(ProgressBar) : indeterminate progress bar

--- lib/components/ProgressBar/ProgressBar.tsx
+++ lib/components/ProgressBar/ProgressBar.tsx
@@ -1,26 +1,43 @@
-// import { useState } from "react";
 import { classMerge } from "../../utils/clsx";
+import styles from "./ProgressBar.module.css";
 import { containerdiv, defaultProgress, defaultProgressBg } from "./constants";
 import { ProgressBarTypes } from "./types";
 
 export default function ProgressBar({
   value,
   label,
+  bgColor,
   color,
-  // variant,
+  variant,
 }: ProgressBarTypes) {
   return (
     <div className={classMerge(containerdiv)}>
-      <div className={classMerge(defaultProgressBg)}>
-        <div
-          className={classMerge(defaultProgress)}
-          style={{
-            transform: `translateX(-${100 - value}%)`,
-            backgroundColor: color,
-          }}
-        ></div>
+      <div
+        className={classMerge(defaultProgressBg)}
+        style={{
+          backgroundColor: bgColor,
+        }}
+      >
+        {variant === "indeterminate" ? (
+          <div
+            className={classMerge(defaultProgress, styles["progress-ind"])}
+            style={{
+              backgroundColor: color,
+            }}
+          ></div>
+        ) : (
+          <div
+            className={classMerge(defaultProgress)}
+            style={{
+              transform: `translateX(-${100 - value}%)`,
+              backgroundColor: color,
+            }}
+          ></div>
+        )}
       </div>
-      {label && <div className="font-semibold text-xs w-7">{value}%</div>}
+      {label && variant === "determinate" && (
+        <div className="font-semibold text-xs w-7">{value}%</div>
+      )}
     </div>
   );
 }
--- lib/components/ProgressBar/types.ts
+++ lib/components/ProgressBar/types.ts
@@ -2,6 +2,7 @@ type variant = "determinate" | "indeterminate";
 
 export type ProgressBarTypes = {
   value: number;
+  bgColor: string;
   color: string;
   variant: variant;
   label: boolean;
