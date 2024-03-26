d8050928f31e5aafe7e0c6ce0d32a537066efcbd
ankushh14 <ankushshenoy97@gmail.com>
2024-03-26 09:33:28 +0530

    chore(ProgressBar) : export progress bar component

--- lib/components/index.ts
+++ lib/components/index.ts
@@ -1,4 +1,5 @@
 import Button from "./Button";
+import ProgressBar from "./ProgressBar";
 import { toast, Toast } from "./Toast";
 
-export { Button, Toast, toast };
+export { Button, ProgressBar, Toast, toast };
